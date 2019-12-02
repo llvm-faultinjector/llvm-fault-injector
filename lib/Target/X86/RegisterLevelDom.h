667545f880b4ec816ecb717712bfd69a65a506ac
rollrat <rollrat@inha.edu>
2019-12-02 21:28:34 +0900

    trace-register: Add RegisterLevelDom header.

---
+++ lib/Target/X86/RegisterLevelDom.h
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+//                LLVM Register Level Dominate Checker for X86
+//
+//===----------------------------------------------------------------------===//
+//
+//  Copyright (C) 2019. rollrat. All Rights Reserved.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef ROLLRAT_REGISTER_LEVEL_DOM_H
+#define ROLLRAT_REGISTER_LEVEL_DOM_H
+
+#include "llvm/CodeGen/MachineFunction.h"
+
+namespace llvm {
+
+class RegisterLevelDom {
+
+};
+
+}
+
+#endif
